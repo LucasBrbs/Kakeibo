f4b00a72ce2c40768a4a6a7863a65bc586443230
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-12-01 11:03:51 -0300

    Initial Commit

---
+++ Kakeibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kakeibo
+//
+//  Created by Lucas Barbosa on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
